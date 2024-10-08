349fc34248f425db4a6b8e9631954a7a3b4fd5c5
mtopf <mtopf@ap2002.chtc.wisc.edu>
2024-10-08 16:10:48 -0500

    lowering resource requirements for minimap

--- nanoCaller.sh
+++ nanoCaller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-NanoCaller --mode snps --bam $1.sort.bam --ref $2 --output $1_output
+NanoCaller --mode all --bam $1.sort.bam --ref $2 --output $1_output
 tar -czf $1_output.tar.gz $1_output
--- vcf-annotator.sh
+++ vcf-annotator.sh
@@ -7,7 +7,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3
 export PATH=$PWD/miniconda3/bin:$PATH
 
-conda install -c bioconda vcf-annotator
+conda install -c bioconda vcf-annotator python=3.12
+
 tar -xf $1_output.tar.gz
 gunzip $1_output/variant_calls.snps.vcf.gz
 cp $_output/variant_calls.snps.vcf .
