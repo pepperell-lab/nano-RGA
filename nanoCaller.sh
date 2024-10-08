e9c6f28e4bcf1a5940b4605ad4bd0d6fd87f1f88
mtopf <mtopf@ap2002.chtc.wisc.edu>
2024-10-08 14:53:28 -0500

    editing nanoCaller and vcf-annotator

--- nanoCaller.sh
+++ nanoCaller.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-NanoCaller --bam $1.sort.bam --ref $2 --output $1_output
+NanoCaller --mode snps --bam $1.sort.bam --ref $2 --output $1_output
+tar -czf $1_output.tar.gz $1_output
--- vcf-annotator.sh
+++ vcf-annotator.sh
@@ -8,5 +8,8 @@ sh Miniconda3-latest-Linux-x86_64.sh -b -p $PWD/miniconda3
 export PATH=$PWD/miniconda3/bin:$PATH
 
 conda install -c bioconda vcf-annotator
+tar -xf $1_output.tar.gz
+gunzip $1_output/variant_calls.snps.vcf.gz
+cp $_output/variant_calls.snps.vcf .
 
 vcf-annotator variant_calls.vcf $2 > $1_annotated.vcf
